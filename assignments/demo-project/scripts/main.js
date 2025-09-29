0e6a6747da0e548628e04c640c0db28c67866240
drewhamer988 <drewhamer988@gmail.com>
2025-09-29 15:42:29 +0000

    9-29-25 Final update for demo-project. patched menu not opening

--- assignments/demo-project/scripts/main.js
+++ assignments/demo-project/scripts/main.js
@@ -1,9 +1,6 @@
 $(document).ready(() => {
     const mobileNav = $('#mobile-nav');
-    const header = $('header');
+    const headerBottomNav = $('#header-bottom-nav');
 
-    mobileNav.click(() => {
-        // When the button is clicked, add or remove the 'nav-open' class on the header
-        header.toggleClass('nav-open');
-});
+    mobileNav.click(() => headerBottomNav.toggle());
 });
