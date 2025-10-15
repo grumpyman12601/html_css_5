e7e69c502e209bac996298368d1ee9bc81acc304
drewhamer988 <drewhamer988@gmail.com>
2025-10-15 16:03:06 +0000

    10-15-25 Midterm

---
+++ demo-midterm-2/javascript/main.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    const mobileNav = $('#mobile-nav');
+    const headerBottomNav = $('#header-bottom-nav');
+
+    mobileNav.click(() => headerBottomNav.toggle());
+});
+
